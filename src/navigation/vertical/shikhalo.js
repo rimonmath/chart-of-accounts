bf93d84e0bedb4ed7e3f6901af44655361f7f9da
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2022-01-28 19:58:55 +0600

    initial commit

---
+++ src/navigation/vertical/shikhalo.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    header: "Shikhalo"
+  },
+
+  {
+    title: "Accounting",
+    icon: "FileTextIcon",
+    children: [
+      {
+        title: "Transactions",
+        route: "apps-invoice-list"
+      },
+      {
+        title: "Reconciliation",
+        route: "apps-invoice-list"
+      },
+      {
+        title: "Chart of Accounts",
+        route: "shikhalo-chart-of-accounts"
+      }
+    ]
+  }
+];
---
+++ src/router/routes/shikhalo.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    path: "/shikhalo/chart-of-accounts",
+    name: "shikhalo-chart-of-accounts",
+    component: () => import("@/views/shikhalo/ChartOfAccounts.vue")
+  }
+];
